8f72d0e98fa5f6ef10fa937673273e448d539a97
Randy Kim <rhkim1292@gmail.com>
2023-04-06 01:16:29 -0700

    Finish styling the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import gifygramLogo from '../images/gifygram-logo.png';
+import { faHouse, faComment } from '@fortawesome/free-solid-svg-icons';
 
-function Navbar(props) {
+function Navbar() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const navLinks = document.querySelectorAll('ul.navbar a');
+        for (const link of navLinks) {
+            if (pathname === '/') {
+                if (link.getAttribute('href') === '/gifygram') {
+                    link.classList.add('active');
+                } else {
+                    link.classList.remove('active');
+                }
+            } else {
+                if (link.getAttribute('href') === '/gifygram' + pathname) {
+                    link.classList.add('active');
+                } else {
+                    link.classList.remove('active');
+                }
+            }
+        }
+    });
+
     return (
         <ul className="navbar">
             <li>
@@ -17,10 +41,20 @@ function Navbar(props) {
                 </div>
             </li>
             <li>
-                <NavLink to="">Home</NavLink>
+                <NavLink to="">
+                    <div className="nav-btn">
+                        <FontAwesomeIcon icon={faHouse} />
+                        Home
+                    </div>
+                </NavLink>
             </li>
             <li>
-                <NavLink to="chat">Chat</NavLink>
+                <NavLink to="chat">
+                    <div className="nav-btn">
+                        <FontAwesomeIcon icon={faComment} />
+                        Chat
+                    </div>
+                </NavLink>
             </li>
         </ul>
     );
